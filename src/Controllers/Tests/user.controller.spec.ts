7c4984e25f73f21ea42eba6b3e57c785da3ff064
victor medeiros <victorehrich@gmail.com>
2022-08-23 14:29:37 -0300

    improvements in controllers unit test, finish service unit test, corrections in controns in controller and service

--- src/Controllers/Tests/user.controller.spec.ts
+++ src/Controllers/Tests/user.controller.spec.ts
@@ -7,6 +7,8 @@ import { UserModel } from 'src/Models/user.model';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
+  
   const userArray:UserModel[] = [{
     id:1,
     name:"Marcos",
@@ -14,8 +16,7 @@ describe('UserController', () => {
     cpf:111,
     email:"test@yahoo.ck",
     password:"senhadeteste"
-  },
-  {
+  },{
     id:2,
     name:"Antônio",
     age:10,
@@ -23,31 +24,30 @@ describe('UserController', () => {
     email:"test@yahoo.ck",
     password:"senhadeteste"
   }]
+
   const mockUserService = {
-    create: jest.fn(dto =>{
+    create: jest.fn().mockImplementation(dto =>{
       let a = {
         id:Date.now(),
         ...dto
       }
       delete a.password
-      return a
+      return Promise.resolve(a)
     }),
-    update:jest.fn().mockImplementation((id,dto)=>{
-      return{
+    update:jest.fn().mockImplementation((id,dto)=>Promise.resolve({
         id,
         ...dto
-      }
-    }),
-    findAll:jest.fn().mockImplementation(()=>{
-      return userArray
-    }),
+    })),
+    
+    findAll:jest.fn().mockResolvedValue(userArray),
+
     findOne:jest.fn().mockImplementation(id=>{
-      return userArray.find(user=>user.id == id)
+      return Promise.resolve(userArray.find(user=>user.id == id))
     }),
     remove:jest.fn().mockImplementation(id=>{
       const user = userArray.indexOf(userArray.find(user=>user.id == id));
       userArray.splice(user,1)
-      return userArray
+      return Promise.resolve(userArray)
     })
   }
 
@@ -58,13 +58,16 @@ describe('UserController', () => {
     }).overrideProvider(UserService).useValue(mockUserService).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 
-  it('should create a User',()=>{
+  describe('create',()=>{
     const createObj:CreateUserDto = {
       name:"Marcos",
       age:10,
@@ -72,61 +75,116 @@ describe('UserController', () => {
       email:"test@yahoo.ck",
       password:"senhadeteste"
     }
-    expect(controller.create(createObj)).toEqual({
-      id: expect.any(Number),
-      name:createObj.name,
-      email:createObj.email,
-      age:createObj.age,
-      cpf:createObj.cpf
+    it('should create a User',async ()=>{
+
+      const result = await controller.create(createObj)
+      expect(result).toEqual({
+        id: expect.any(Number),
+        name:createObj.name,
+        email:createObj.email,
+        age:createObj.age,
+        cpf:createObj.cpf
+      })
+      expect(mockUserService.create).toHaveBeenCalledWith(createObj)
     })
-    expect(mockUserService.create).toHaveBeenCalledWith(createObj)
+
+    it('should throw an exception', () => {
+      // Arrange
+      jest.spyOn(mockUserService, 'create').mockRejectedValueOnce(new Error());
+
+      // Assert
+      expect(controller.create(createObj)).rejects.toThrowError();
+    });
   })
-  it('should update an Users',()=>{
+  describe('update',()=>{
+
     const updateObj:UpdateUserDto = {
       name:"Marcos",
       age:10,
       cpf:111,
       password:"senhadeteste"
     }
-    expect(controller.update("1",updateObj)).toEqual({
-      id: 1,
-      ...updateObj
+
+    it('should update a User', async () => {
+      const result = await controller.update("1",updateObj)
+
+      expect(result).toEqual({
+        id: 1,
+        ...updateObj
+      });
+      expect(mockUserService.update).toHaveBeenCalledTimes(1);
+      //expect(mockUserService.update).toHaveBeenCalledWith("1",updateObj)
+  });
+
+    it('should throw an exception', () => {
+      // Arrange
+      jest.spyOn(mockUserService, 'update').mockRejectedValueOnce(new Error());
+
+      // Assert
+      expect(controller.update("1",updateObj)).rejects.toThrowError();
     });
-    expect(mockUserService.update).toHaveBeenCalled()
+
+  })
+  describe("findAll",()=>{
+    it('should find all Users',async ()=>{
+      expect(await controller.findAll()).toEqual(
+        userArray 
+      );
+      expect(mockUserService.findAll).toHaveBeenCalled()
+    })
+    it('should throw an exception', () => {
+      // Arrange
+      jest.spyOn(mockUserService, 'findAll').mockRejectedValueOnce(new Error());
+
+      // Assert
+      expect(controller.findAll()).rejects.toThrowError();
+    });
+  })
+  describe("findOne",()=>{
+    it('Should find a user by id',async ()=>{
+      expect(await controller.findOne("2")).toEqual(
+        {
+          id:2,
+          name:"Antônio",
+          age:10,
+          cpf:113,
+          email:"test@yahoo.ck",
+          password:"senhadeteste"
+        }
+      )
+      expect(mockUserService.findOne).toHaveBeenCalled()
+      expect(mockUserService.findOne).toHaveBeenCalledWith(2)
+    })
+    
+    it('should throw an exception', () => {
+      // Arrange
+      jest.spyOn(mockUserService, 'findOne').mockRejectedValueOnce(new Error());
+
+      // Assert
+      expect(controller.findOne("2")).rejects.toThrowError();
+    });
   })
 
-  it('should find all Users',()=>{
-    expect(controller.findAll()).toEqual(
-      userArray 
-    );
-    expect(mockUserService.findAll).toHaveBeenCalled()
-  })
+  describe("Remove", ()=>{
+    it('Should remove a user by id',async ()=>{
+      expect(await controller.remove("1")).toEqual(
+        [{
+          id:2,
+          name:"Antônio",
+          age:10,
+          cpf:113,
+          email:"test@yahoo.ck",
+          password:"senhadeteste"
+        }]
+      )
+      expect(mockUserService.remove).toHaveBeenCalled()
+    })
+    it('should throw an exception', () => {
+      // Arrange
+      jest.spyOn(mockUserService, 'remove').mockRejectedValueOnce(new Error());
 
-  it('Should find a user by id',()=>{
-    expect(controller.findOne("2")).toEqual(
-      {
-        id:2,
-        name:"Antônio",
-        age:10,
-        cpf:113,
-        email:"test@yahoo.ck",
-        password:"senhadeteste"
-      }
-    )
-    expect(mockUserService.findOne).toHaveBeenCalled()
-  })
-  
-  it('Should remove a user by id',()=>{
-    expect(controller.remove("1")).toEqual(
-      [{
-        id:2,
-        name:"Antônio",
-        age:10,
-        cpf:113,
-        email:"test@yahoo.ck",
-        password:"senhadeteste"
-      }]
-    )
-    expect(mockUserService.remove).toHaveBeenCalled()
-  })
+      // Assert
+      expect(controller.remove("2")).rejects.toThrowError();
+    });
+  })  
 });
--- src/Controllers/user.controller.ts
+++ src/Controllers/user.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, Res, HttpStatus } from '@nestjs/common';
 
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
-import { UserService } from 'src/Services/user.service';
-import { UpdateUserDto } from 'src/Models/UpdateDTO/update-user.dto.model';
-import { CreateUserDto } from 'src/Models/CreateDTO/create-user.dto.model';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { UpdateUserDto } from '../Models/UpdateDTO/update-user.dto.model';
+import { CreateUserDto } from '../Models/CreateDTO/create-user.dto.model';
+import { UserService } from '../Services/user.service';
 
 @ApiTags('UserController')
 @Controller('user')
@@ -12,31 +12,31 @@ export class UserController {
 
   @ApiCreatedResponse({ description: 'The User has been successfully created.'})
   @Post()
-  create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
-    const newUser = this.userService.create(createUserDto);
+  async create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    const newUser = await this.userService.create(createUserDto);
     return newUser
   }
 
+  @ApiOkResponse({description: 'The Users were found.'})
   @Get()
-  async findAll(@Res() response,) {
+  async findAll() {
     const all = await this.userService.findAll()
-    return response.status(HttpStatus.OK).send({
-      Users:[all]
-    }) 
+    return all
   }
 
+  @ApiOkResponse({description: 'The User was found.'})
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.userService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.userService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.userService.remove(+id);
   }
 }
